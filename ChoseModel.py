cf72517f02bc8b320584619ba0fb7814879006cf
nrot <nrot13@gmail.com>
2015-05-30 15:42:06 +0300

    Add sys.path. Now this work on all systems/

--- ChoseModel.py
+++ ChoseModel.py
@@ -1,31 +1,35 @@
 __author__ = 'nrot'
 
 import random
-import Sites.BadFon
-import Sites.Wall
-import Sites.Zastavki
-import Sites.GoodFon
-
+import sys
+import os
+sys.path.insert(2, os.getcwd() + '\\Sites')
+import Wall
+import GoodFon
+import BadFon
+import Zastavki
 
 def Chose(LogFile, Options):
+
+        print(sys.path)
         site = random.randint(1, 4)
         if site == 1:
-            def_img = Sites.GoodFon.GoodFon(LogFile, Options)
+            def_img = GoodFon.GoodFon(LogFile, Options)
             if def_img == 'Error connect':
                 LogFile.write(aType='connect', text='wall.alphacoders.com', time=int(Options.TimeToSleep/60))
                 return 'Error connect'
         elif site == 2:
-            def_img = Sites.Wall.Wall(LogFile, Options)
+            def_img = Wall.Wall(LogFile, Options)
             if def_img == 'Error connect':
                 LogFile.write(aType='connect', text='www.goodfon.ru', time=int(Options.TimeToSleep/60))
                 return 'Error connect'
         elif site == 3:
-            def_img = Sites.BadFon.BadFon(LogFile, Options)
+            def_img = BadFon.BadFon(LogFile, Options)
             if def_img == 'Error connect':
                 LogFile.write(aType='connect', text='www.badfon.ru', time=int(Options.TimeToSleep/60))
                 return 'Error connect'
         elif site == 4:
-            def_img = Sites.Zastavki.Zastavki(LogFile, Options)
+            def_img = Zastavki.Zastavki(LogFile, Options)
             if def_img == 'Error connect':
                 LogFile.write(aType='connect', text='www.zastavki.com', time=int(Options.TimeToSleep/60))
                 return 'Error connect'
